b27210b9cb30a8cfca0fa08766b5e9977bcad724
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-28 11:06:31 +0200

    kaasSMKWBHDJGVEv

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,4 +1,7 @@
-	<!DOCTYPE html>
+<?php
+$path = "//~speurtocht//";
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -10,27 +13,39 @@
 	<meta content="" name="keywords">
 
 	<!-- Favicons -->
-	<link href="/~fp241754/scav-hunt/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	<?php
+	echo '
+	<link href="' . $path . 'assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	';
+	?>
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 	<!-- Vendor CSS Files -->
-	<link href="/~fp241754/scav-hunt/assets/vendor/aos/aos.css" rel="stylesheet">
-	<link href="/~fp241754/scav-hunt/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/~fp241754/scav-hunt/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-	<link href="/~fp241754/scav-hunt/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-	<link href="/~fp241754/scav-hunt/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-	<link href="/~fp241754/scav-hunt/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	<?php
+	echo '
+	<link href="' . $path . 'assets/vendor/aos/aos.css" rel="stylesheet">
+	<link href="' . $path . 'assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="' . $path . 'assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="' . $path . 'assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="' . $path . 'assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="' . $path . 'assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	';
+	?>
 
 	<script src="https://kit.fontawesome.com/51b9333b7a.js" crossorigin="anonymous"></script>
 
 	<!-- Main CSS File -->
-	<link href="/~fp241754/scav-hunt/assets/css/style.css" rel="stylesheet">
+	<?php
+	echo '
+	<link href="' . $path . 'assets/css/style.css" rel="stylesheet">
+	';
+	?>
 </head>
 
 <body>
-	<div id="box"> <?php include_once("/~fp241754/scav-hunt/filedir.php") ?> </div>
+	<div id="box"> <?php include_once("" . $path . "/filedir.php") ?> </div>
 	<i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
 
 	<div class="header-box"></div>
@@ -39,12 +54,16 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-1">
-					<a href="/~fp241754/scav-hunt/"><img src="/~fp241754/scav-hunt/assets/img/1234.png" alt="" class="img-fluid rounded-circle"></a>
+					<?php
+					echo '
+					<a href="' . $path . '"><img src="' . $path . 'assets/img/1234.png" alt="" class="img-fluid rounded-circle"></a>
+					';
+					?>
 				</div>
 
 				<div class="col-md-2">
 					<div class="profile">
-						<h1 class="text-light"><a href="/~fp241754/scav-hunt/">Scav Hunt</a></h1>
+						<h1 class="text-light"><a href="<?php echo $path?>">Scav Hunt</a></h1>
 						<div class="social-links mt-3 text-center">
 							<a href="" target="_blank"><i class="fa fa-info" aria-hidden="true"></i></a>
 							<a href="" target="_blank"><i class="fa fa-address-book" aria-hidden="true"></i></a>
@@ -56,33 +75,38 @@
 						<?php session_start(); ?>
 					<nav id="navbar" class="nav-menu navbar">
 						<ul class="nav-menu">
-							<li><a href="/~fp241754/scav-hunt/" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>
+							<li><a href="<?php echo $path?>" class="nav-link scrollto hover-sound"><i class="bx bx-home"></i> Home</a></li>
 							<?php
 							if (isset($_SESSION['docent'])) {
 								// echo <<< bar
-								echo '<li><a href="/~fp241754/scav-hunt/docent/"><i class="bx bx-user"></i> Docent</a></li>';
+								echo '<li><a href="<?php echo $path?>docent/"><i class="bx bx-user"></i> Docent</a></li>';
 								echo '<li class="dropdown"><a href="#"><i class="bx bx-user"></i> Beheren <i class="bx bx-chevron-down"></i></a>';
 								echo 	'<ul>';
-								echo 		'<li><a href="/~fp241754/scav-hunt/docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
-								echo 		'<li><a href="/~fp241754/scav-hunt/docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
-								echo 		'<li><a href="/~fp241754/scav-hunt/docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
-								echo 		'<li><a href="/~fp241754/scav-hunt/docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
-								echo 		'<li><a href="/~fp241754/scav-hunt/docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
+								echo 		'<li><a href="' . $path . 'docent/groepje-tonen.php"><i class="bx bx-group"></i> Groepjes</a></li>';
+								echo 		'<li><a href="' . $path . 'docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
+								echo 		'<li><a href="' . $path . 'docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
+								echo 		'<li><a href="' . $path . 'docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
+								echo 		'<li><a href="' . $path . 'docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
 								if (isset($_SESSION['admin']))
 									echo	'<li><a href="docent_toevoegen.php"><i class="bx bx-user"></i> Docent toevoegen</a></li>';
 								echo 	'</ul>';
 								echo '</li>';
-								// echo '<li><a href="/~fp241754/scav-hunt/login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
+								// echo '<li><a href="' . $path . 'login/logout.php"><i class="bx bx-user"></i> Uitloggen</a></li>';
 								// bar;
 							} else if (isset($_SESSION['pagina']) && $_SESSION['pagina'] == 'home') {
-								echo '<li><a href="/~fp241754/scav-hunt/login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
-								echo '<li><a href="/~fp241754/scav-hunt/student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
+								echo '<li><a href="' . $path . 'login" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Docent login</a></li>';
+								echo '<li><a href="' . $path . 'student_code.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> Student login</a></li>';
 							}
 							?>
-							<li><a href="/~fp241754/scav-hunt/about.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>
+							<?php
+							echo '
+							<li><a href="' . $path . 'about.php" class="nav-link scrollto hover-sound"><i class="bx bx-user"></i> About</a></li>
+							';
+							echo '
 						</ul>
 					</nav>
 				</div>
 			</div>
 		</div>
 	</header>
+	';
