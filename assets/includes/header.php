23c667a9c4c06bdd439705f504fb491cc92a6ec6
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2023-06-28 11:44:28 +0200

    Header update

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -45,8 +45,8 @@ $path = "~speurtocht/";
 </head>
 
 <body>
-	<div id="box"> <?php include_once("" . $path . "filedir.php") ?> </div>
-	<i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+	<!-- <div id="box"> <?php include_once("" . $path . "filedir.php") ?> </div>
+	<i class="bi bi-list mobile-nav-toggle d-xl-none"></i> -->
 
 	<div class="header-box"></div>
 
@@ -92,7 +92,7 @@ $path = "~speurtocht/";
 								echo 		'<li><a href="/' . $path . 'docent/winnaar-tonen.php"><i class="bx bx-trophy"></i> Winnaar</a></li>';
 								echo 		'<li><a href="/' . $path . 'docent/koter_analyzer.php"><i class="bx bx-map"></i> Locaties</a></li>';
 								echo 		'<li><a href="/' . $path . 'docent/vragen-aanpassen.php"><i class="bx bx-edit"></i> Vragen bijwerken</a></li>';
-								echo 		'<li><a href="/' . $path . 'docent/code-tonen.php"><i class="bx bx-code"></i> Code tonen</a></li>';
+								echo 		'<li><a href="/' . $path . 'docent/unkieke_code_generatie.php"><i class="bx bx-code"></i> Code genereren</a></li>';
 								if (isset($_SESSION['admin']))
 									echo	'<li><a href="docent_toevoegen.php"><i class="bx bx-user"></i> Docent toevoegen</a></li>';
 								echo 	'</ul>';
